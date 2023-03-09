b45780f55ffc7ad5f6bdb69cc94c73df2bce2f80
IDGS-804-19002061 <19002061@alumnos.utleon.edu.mx>
2023-03-08 19:36:05 -0600

    Base de datos, ELiminar, Modificar, 08/03/2023

--- main.py
+++ main.py
@@ -16,9 +16,61 @@ def index():
                       email=create_form.email.data)
         db.session.add(alum)
         db.session.commit()
+        return redirect(url_for('ABCompleto'))
     return render_template('index.html', form = create_form)
 
+@app.route("/ABCompleto", methods=['GET','POST'])
+def ABCompleto():
+    create_form=forms.UserForm(request.form)
+    alumnos = Alumnos.query.all()
+    return render_template('AbCompleto.html', form=create_form, alumnos=alumnos)
 
+
+@app.route("/modificar", methods=['GET','POST'])
+def modificar():
+    create_form=forms.UserForm(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=id
+        create_form.nombre.data=alum1.nombre
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+    
+    if request.method=='POST':
+        id=create_form.id.data
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum.nombre=create_form.nombre.data
+        alum.apaterno=create_form.apaterno.data
+        alum.email=create_form.email.data
+        db.session.add(alum)
+        db.session.commit()
+
+        return redirect(url_for('ABCompleto'))
+
+    return render_template('modificar.html', form=create_form)
+
+
+@app.route('/eliminar', methods=['GET', 'POST'])
+def eliminar():
+    create_form=forms.UserForm(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=id
+        create_form.nombre.data=alum1.nombre
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+
+    if request.method=='POST':
+        id=create_form.id.data
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        db.session.delete(alum)
+        db.session.commit()
+
+        return redirect(url_for('ABCompleto'))
+
+    return render_template('eliminar.html', form=create_form, id=id)
 
 if __name__=='__main__':
     csrf.init_app(app)
